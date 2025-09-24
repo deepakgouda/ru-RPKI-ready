cf3220a2b661b486e03376a7b8e60f03a419dcff
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2025-09-23 21:24:19 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -63,8 +63,8 @@ class IPLookupService:
             os.getcwd(), "data", f"prefix_tags_{self.curr_date}_v4.parquet"
         )
         print(f"Loading data from {path_pfx2org_v4}")
-        self.df_pfx2org = pd.read_parquet(path_pfx2org_v4)
-        self.df_pfx2org["af"] = 4
+        self.df_v4 = pd.read_parquet(path_pfx2org_v4)
+        self.df_v4["af"] = 4
 
         path_pfx2org_v6 = os.path.join(
             os.getcwd(), "data", f"prefix_tags_{self.curr_date}_v6.parquet"
@@ -73,7 +73,7 @@ class IPLookupService:
         self.df_v6 = pd.read_parquet(path_pfx2org_v6)
         self.df_v6["af"] = 6
 
-        self.df_pfx2org = pd.concat([self.df_pfx2org, self.df_v6], ignore_index=True)
+        self.df_pfx2org = pd.concat([self.df_v4, self.df_v6], ignore_index=True)
         
         self.df_pfx2org["origin_asn"] = "AS" + self.df_pfx2org["origin_asn"].astype(str)
         self.df_pfx2org["asn_cluster"] = "AS" + self.df_pfx2org["asn_cluster"].astype(str)
